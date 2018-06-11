a7f6d366fd2407fbdfbffc9b4b90de062a529c83
ferrad.fm@gmail.com <ferrad.fm@gmail.com>
2018-06-11 21:29:00 +0200

    TreeView of all drives and files

--- WPFTreeView/MainWindow.xaml.cs
+++ WPFTreeView/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,5 +25,145 @@ namespace WPFTreeView
         {
             InitializeComponent();
         }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Get every logical drive on the machine
+            foreach (var drive in Directory.GetLogicalDrives())
+            {
+                // Create a new item for it 
+                var item = new TreeViewItem()
+                {
+                    // Set the header 
+                    Header = drive,
+
+                    // And full path 
+                    Tag = drive
+                };
+
+                // Add dummy item
+                item.Items.Add(null);
+
+                // Listen out for item being expanded 
+                item.Expanded += Folder_Expanded;
+
+                // Add it to the main tree view
+                FolderView.Items.Add(item);
+            }
+        }
+
+        private void Folder_Expanded(object sender, RoutedEventArgs e)
+        {
+            #region Checks
+
+            var item = (TreeViewItem)sender;
+
+            // If the item only contains the dummy data
+            if (item.Items.Count != 1 || item.Items[0] != null)
+                return;
+
+            // Clear dummy data
+            item.Items.Clear();
+
+            // Get fullpath 
+            var fullpath = (string)item.Tag;
+
+            #endregion
+
+            #region Directories
+
+            //Create a blank list for directories
+            var directories = new List<string>();
+
+            // Try and get directories from the folder
+            // ignoring any issues doing so
+
+            try
+            {
+                var dirs = Directory.GetDirectories(fullpath);
+
+                if (dirs.Length > 0)
+                    directories.AddRange(dirs);
+            }
+            catch { }
+            
+            // For each directory
+            directories.ForEach(directoryPath =>
+            {
+                // Create a directory item 
+                var subItem = new TreeViewItem()
+                {
+                    // Set header as folder name
+                    Header = GetFileFolderName(directoryPath),
+                    // And tag as full path
+                    Tag = directoryPath
+                };
+
+                // Add dummy item so we can expand folder
+                subItem.Items.Add(null);
+
+                // Handle expanding
+                subItem.Expanded += Folder_Expanded;
+
+                //Add this item to the parent
+                item.Items.Add(subItem);
+            });
+            #endregion
+
+            #region Get files
+
+            //Create a blank list for directories
+            var files = new List<string>();
+
+            // Try and get files from the folder
+            // ignoring any issues doing so
+
+            try
+            {
+                var fs = Directory.GetFiles(fullpath);
+
+                if (fs.Length > 0)
+                    files.AddRange(fs);
+            }
+            catch {}
+
+            // For each file...
+            files.ForEach(filePath =>
+            {
+                // Create a file item 
+                var subItem = new TreeViewItem()
+                {
+                    // Set header as folder name
+                    Header = GetFileFolderName(filePath),
+                    // And tag as full path
+                    Tag = filePath
+                };
+
+                //Add this item to the parent
+                item.Items.Add(subItem);
+            });           
+        }
+        #endregion
+
+        public static string GetFileFolderName(string path)
+        {
+            // C : \Something\a file
+            // C : \Something\a file.png
+
+            // If we have no path return empty
+            if (string.IsNullOrWhiteSpace(path))
+                return string.Empty;
+
+            // Make all slashes back slashes
+            var normalizedPath = path.Replace('/', '\\');
+
+            // Find the last backslash in the path
+            var lastIndex = normalizedPath.LastIndexOf('\\');
+
+            // If we don't find a back slash return the path itself
+            return path.Substring(lastIndex + 1);
+        }
+
+        
     }
 }
